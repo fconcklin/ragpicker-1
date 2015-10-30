6a8e357f07a6d2d03bc9b5d8b3debd219c2459d9
intelemetry <intelemetry@intelemetrys-MacBook-Pro.local>
2015-10-29 23:24:16 -0700

    updates

--- setupvm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-vboxmanage modifyvm ragpicker_default_1445068829820_8082 --usb off
-
-# there is a problem with vagrant up so start it manually
-vboxmanage startvm ragpicker_default_1445068829820_8082 --type headless 
